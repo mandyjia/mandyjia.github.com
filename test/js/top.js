111ffddca2cf2a59131b4e6de854cadfe6d1f149
unknown <xx@xx1il>
2017-02-15 16:39:07 +0800

    jia

---
+++ test/js/top.js
@@ -0,0 +1,10 @@
+function getPos(obj){
+    var l = 0;
+    var t = 0;
+    while(obj){
+        l+=obj.offsetLeft;
+        t+=obj.offsetTop;
+        obj=obj.offsetParent;
+    }
+    return {left:l,top:t};
+}            
