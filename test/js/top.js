0de575cfe6f2dc0de7af99de3c830be9a4afc763
unknown <xx@xx1il>
2017-02-16 17:43:21 +0800

    jia

--- test/js/top.js
+++ test/js/top.js
@@ -8,3 +8,21 @@ function getPos(obj){
     }
     return {left:l,top:t};
 }            
+window.onload = function () {
+var oItem = document.getElementById('top');
+var oItem2 = document.getElementById('top2');
+var top = getPos(oItem).top;
+	window.onscroll=function(){
+	    var scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
+	    if(scrollTop>= top){
+	        //吸住
+	        oItem.style.position='fixed';
+	        oItem.style.top='0';
+	        oItem2.style.display='block';
+	    }else{
+	        //回去
+	        oItem.style.position='static';
+	        oItem2.style.display='none';
+	    }
+	};
+}
