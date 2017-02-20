f327dff51c8edb7561d71c3ffd1c396665036ab9
unknown <xx@xx1il>
2017-02-20 15:56:02 +0800

    jia

--- test7/js/common.js
+++
@@ -1,7 +0,0 @@
-(function(win,doc){
-	function change(){
-		doc.documentElement.style.fontSize = 20/320*doc.documentElement.clientWidth+'px';					
-	}
-	change();
-	win.addEventListener('resize',change,false);
-})(window,document);
